cf663899379ec99e07c296dabb5c524bce0b4abc
Amarmanhar <58129626+Amarmanhar@users.noreply.github.com>
2023-07-08 14:43:46 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,25 @@
-const http = require('http');
+// const http = require('http');
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
-console.log(routes.sometext);
- const server = http.createServer(routes.handler);
- server.listen(3000);
+// console.log(routes.sometext);
+//  const server = http.createServer(routes.handler);
+//  server.listen(3000);
+
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) =>{
+  console.log("in the middleware");
+  next();
+})
+
+app.use((req, res, next) =>{
+  console.log('i am in another middleware');
+  res.send('<h1> hello form express js</h1>')
+})
+
+
+app.listen(3000);
