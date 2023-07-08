c0bb85b1cdd4f497735deda3a941badc15e1f11d
Amarmanhar <58129626+Amarmanhar@users.noreply.github.com>
2023-07-08 21:33:21 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -30,24 +30,17 @@ const express = require('express');
 const bodyParser = require("body-parser");
 
 const app = express();
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended : false}))
 
-app.use('/add-product', (req,res,next) =>{
-    res.send('<form action="/product" method="POST"><input type ="text" name="title"><input type="number" name="size" placeholder="enter size"></br><button type ="submit">ADD PRODUCT</button></form>');
+app.use(adminRouter);
+app.use(shopRouter);
+
+app.use((req,res,next)=>{
+   res.status(404).send('<h1> Page Not Found </h1>')
 })
-
-app.post('/product', (req,res,next)=>{
-      console.log(req.body);
-      res.redirect('/');
-})
-
-app.use("/",(req, res, next) =>{
-  
-  res.send('<h1> hello form express js</h1>')
-})
-
-
 
 //  const server = http.createServer(app);
 //  server.listen(3000);
